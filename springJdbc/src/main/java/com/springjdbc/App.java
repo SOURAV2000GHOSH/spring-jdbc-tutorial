5d534810cc72613a5120f4d7cf87ff34cece22fd
Sourav <souravghosh6543@gmail.com>
2023-12-21 14:42:53 +0530

    adding code for inserting data in database

--- springJdbc/src/main/java/com/springjdbc/App.java
+++ springJdbc/src/main/java/com/springjdbc/App.java
@@ -1,7 +1,19 @@
 package com.springjdbc;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+	
+	public static void main(String[] args) {
+    System.out.println("Application started...........");
+    AbstractApplicationContext context= new ClassPathXmlApplicationContext("com/springjdbc/config.xml");
+    JdbcTemplate template = context.getBean("jdbctemplate",JdbcTemplate.class);
+//   insert query
+    String query="insert into student(id,name,city) values(?,?,?)";
+    int result=template.update(query,101,"Riya Majumder","Harinchawra");
+    System.out.println("number of rows of updated : "+result);
+    
   }
 }
